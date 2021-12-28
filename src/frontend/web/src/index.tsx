613646e3b15682affb848ad129e48041d8e78fb9
chahtk <chahtk@gmail.com>
2021-12-28 13:51:38 +0900

    feat: globalstyle 세팅

--- src/frontend/web/src/index.tsx
+++ src/frontend/web/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import GlobalStyle from './styles/GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),

0c381c0545bda2b2ca8501d3dd265ae47ced2d66
njsh4261 <njsh4261@snu.ac.kr>
2021-12-28 10:55:50 +0900

    feat: 채팅 프로토타입 프로젝트 폴더 초기화

---
+++ src/backend/chat-proto/src/test/java/com/lastpunch/chatproto/ChatProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastpunch.chatproto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatProtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

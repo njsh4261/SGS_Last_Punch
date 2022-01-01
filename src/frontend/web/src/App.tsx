10b4df57a35cd009d5ac14a7ac579f452ce006ff
chahtk <chahtk@gmail.com>
2022-01-01 19:04:51 +0900

    feat: header prototype

--- src/frontend/web/src/App.tsx
+++ src/frontend/web/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Chat from './components/Chat';
+import Header from './components/Header';
 
 function App() {
-  return <Chat></Chat>;
+  return (
+    <>
+      <Header></Header>
+      {/* <Chat></Chat>; */}
+    </>
+  );
 }
 
 export default App;
---
+++ src/frontend/web/src/components/Header.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import icon from '../icon/restore.svg';
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  background-color: beige;
+  padding: 6px 0px;
+`;
+
+const Hside = styled.article`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-left: 16px;
+  padding-right: 20px;
+`;
+
+const HhistoryIcon = styled.article`
+  width: 24px;
+  height: 24px;
+  background-image: url(${icon});
+  background-repeat: no-repeat;
+`;
+
+const Hsearch = styled.article`
+  flex: 2;
+`;
+
+const Hright = styled.article`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <Hside>
+        <HhistoryIcon></HhistoryIcon>
+      </Hside>
+      <Hsearch>search</Hsearch>
+      <Hright>memeber</Hright>
+    </HeaderContainer>
+  );
+};
+
+export default Header;

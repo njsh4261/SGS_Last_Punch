7e08bc8b2b7937490098a0b7cbcbe1dcceea4150
chahtk <chahtk@google.com>
2022-01-03 10:45:46 +0900

    feat: header prototype

--- src/frontend/web/src/App.tsx
+++ src/frontend/web/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Chat from './components/Chat';
+import MainHeader from './components/MianHeader';
 
 function App() {
-  return <Chat></Chat>;
+  return (
+    <>
+      <MainHeader></MainHeader>
+      {/* <Chat></Chat>; */}
+    </>
+  );
 }
 
 export default App;

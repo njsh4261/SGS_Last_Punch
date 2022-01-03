c259fe7589ed5df07a4b3d27744aafaa57163ccc
Jihyo Kim <njsh4261@gmail.com>
2022-01-03 11:53:49 +0900

    Revert "Chore: MySQL 스키마 스크립트 및 이미지 수정 (#31)"
    This reverts commit 2b9db17c26e5941706621364adc1e948304b8bb8.

--- scripts/LastPunch_schema.sql
+++ scripts/LastPunch_schema.sql
@@ -1,3 +1,14 @@
+CREATE TABLE `workspace`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `description` TEXT NULL,
+    `language` CHAR(255) NOT NULL,
+    `settings` TINYINT NOT NULL,
+    `status` TINYINT NOT NULL,
+    `created_dt` DATETIME NOT NULL,
+    `modified_dt` DATETIME NOT NULL
+);
+
 CREATE TABLE `member`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `email` VARCHAR(255) NOT NULL UNIQUE,
@@ -17,30 +28,15 @@ CREATE TABLE `member`(
     `last_login_dt` DATETIME NOT NULL
 );
 
-CREATE TABLE `workspace`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `description` TEXT NULL,
-    `language` CHAR(255) NOT NULL,
-    `settings` TINYINT NOT NULL,
-    `status` TINYINT NOT NULL,
-    `created_dt` DATETIME NOT NULL,
-    `modified_dt` DATETIME NOT NULL
-);
-
 CREATE TABLE `member_workspace`(
-    `member_id` BIGINT UNSIGNED NOT NULL,
+    `member_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `workspace_id` BIGINT UNSIGNED NOT NULL,
-    PRIMARY KEY (`member_id`, `workspace_id`)
+	INDEX (`workspace_id`)
 );
-
-ALTER TABLE `member_workspace` ADD CONSTRAINT `member_workspace_member_id_foreign` FOREIGN KEY(`member_id`) REFERENCES `member`(`id`);
-ALTER TABLE `member_workspace` ADD CONSTRAINT `member_workspace_workspace_id_foreign` FOREIGN KEY(`workspace_id`) REFERENCES `workspace`(`id`);
 
 CREATE TABLE `channel`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `workspace_id` BIGINT UNSIGNED NOT NULL,
-    `creator_id` BIGINT UNSIGNED NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `topic` VARCHAR(255) NULL,
     `description` TEXT NULL,
@@ -49,68 +45,18 @@ CREATE TABLE `channel`(
     `status` TINYINT NOT NULL,
     `created_dt` DATETIME NOT NULL,
     `modified_dt` DATETIME NOT NULL,
-	INDEX (`workspace_id`),
-    INDEX (`creator_id`)
-);
-
-ALTER TABLE `channel` ADD CONSTRAINT `channel_workspace_id_foreign` FOREIGN KEY(`workspace_id`) REFERENCES `workspace`(`id`);
-ALTER TABLE `channel` ADD CONSTRAINT `channel_creator_id_foreign` FOREIGN KEY(`creator_id`) REFERENCES `member`(`id`);
-
-CREATE TABLE `member_channel`(
-    `member_id` BIGINT UNSIGNED NOT NULL,
-    `channel_id` BIGINT UNSIGNED NOT NULL,
-	PRIMARY KEY (`member_id`, `channel_id`)
-);
-
-ALTER TABLE `member_channel` ADD CONSTRAINT `member_channel_member_id_foreign` FOREIGN KEY(`member_id`) REFERENCES `member`(`id`);
-ALTER TABLE `member_channel` ADD CONSTRAINT `member_channel_channel_id_foreign` FOREIGN KEY(`channel_id`) REFERENCES `channel`(`id`);
-
-CREATE TABLE `message`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `channel_id` BIGINT UNSIGNED NOT NULL,
-    `content` TEXT NOT NULL,
-    `refer_reply_id` BIGINT UNSIGNED NULL,
-    `file_id` BIGINT NULL,
-    `status` TINYINT NOT NULL,
-    `created_dt` DATETIME NOT NULL,
-    `modified_dt` DATETIME NOT NULL,
-    `reply_count` INT NOT NULL,
-    `last_reply_dt` DATETIME NOT NULL,
-	INDEX (`channel_id`)
+	INDEX (`workspace_id`)
 );
 
 CREATE TABLE `reply`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `message_id` BIGINT UNSIGNED NOT NULL,
     `content` TEXT NOT NULL,
-    `file_id` BIGINT UNSIGNED NULL,
     `status` TINYINT NOT NULL,
     `created_dt` DATETIME NOT NULL,
     `modified_dt` DATETIME NOT NULL,
 	INDEX (`message_id`)
 );
-
-CREATE TABLE `file`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    `description` TEXT NOT NULL,
-    `content` BLOB NOT NULL
-);
-
-ALTER TABLE `message` ADD CONSTRAINT `message_channel_id_foreign` FOREIGN KEY(`channel_id`) REFERENCES `channel`(`id`);
-ALTER TABLE `message` ADD CONSTRAINT `message_refer_reply_id_foreign` FOREIGN KEY(`refer_reply_id`) REFERENCES `reply`(`id`);
-ALTER TABLE `message` ADD CONSTRAINT `message_file_id_foreign` FOREIGN KEY(`file_id`) REFERENCES `file`(`id`);
-ALTER TABLE `reply` ADD CONSTRAINT `reply_message_id_foreign` FOREIGN KEY(`message_id`) REFERENCES `message`(`id`);
-ALTER TABLE `reply` ADD CONSTRAINT `reply_file_id_foreign` FOREIGN KEY(`file_id`) REFERENCES `file`(`id`);
-
-CREATE TABLE `member_message`(
-    `member_id` BIGINT UNSIGNED NOT NULL,
-    `message_id` BIGINT UNSIGNED NOT NULL,
-	PRIMARY KEY (`member_id`, `message_id`)
-);
-
-ALTER TABLE `member_message` ADD CONSTRAINT `member_message_member_id_foreign` FOREIGN KEY(`member_id`) REFERENCES `member`(`id`);
-ALTER TABLE `member_message` ADD CONSTRAINT `member_message_message_id_foreign` FOREIGN KEY(`message_id`) REFERENCES `message`(`id`);
 
 CREATE TABLE `notification`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -123,10 +69,41 @@ CREATE TABLE `notification`(
 );
 
 CREATE TABLE `member_notification`(
-    `member_id` BIGINT UNSIGNED NOT NULL,
+    `member_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `notification_id` BIGINT UNSIGNED NOT NULL,
-	PRIMARY KEY (`member_id`, `notification_id`)
+	INDEX (`notification_id`)
 );
 
-ALTER TABLE `member_notification` ADD CONSTRAINT `member_notification_member_id_foreign` FOREIGN KEY(`member_id`) REFERENCES `member`(`id`);
+CREATE TABLE `message`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `channel_id` BIGINT UNSIGNED NOT NULL,
+    `content` TEXT NOT NULL,
+    `refer_reply_id` BIGINT UNSIGNED NULL,
+    `status` TINYINT NOT NULL,
+    `created_dt` DATETIME NOT NULL,
+    `modified_dt` DATETIME NOT NULL,
+    `reply_count` INT NOT NULL,
+    `last_reply_dt` DATETIME NOT NULL,
+	INDEX (`channel_id`)
+);
+
+CREATE TABLE `channel_member`(
+    `channel_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `member_id` BIGINT UNSIGNED NOT NULL, 
+	INDEX (`member_id`)
+);
+
+CREATE TABLE `message_user`(
+    `message_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `member_id` BIGINT UNSIGNED NOT NULL,
+	INDEX (`member_id`)
+);
+
+ALTER TABLE `channel` ADD CONSTRAINT `channel_workspace_id_foreign` FOREIGN KEY(`workspace_id`) REFERENCES `workspace`(`id`);
+ALTER TABLE `member_workspace` ADD CONSTRAINT `member_workspace_workspace_id_foreign` FOREIGN KEY(`workspace_id`) REFERENCES `workspace`(`id`);
+ALTER TABLE `channel_member` ADD CONSTRAINT `channel_member_member_id_foreign` FOREIGN KEY(`member_id`) REFERENCES `member`(`id`);
+ALTER TABLE `message_user` ADD CONSTRAINT `message_user_member_id_foreign` FOREIGN KEY(`member_id`) REFERENCES `member`(`id`);
+ALTER TABLE `message` ADD CONSTRAINT `message_channel_id_foreign` FOREIGN KEY(`channel_id`) REFERENCES `channel`(`id`);
+ALTER TABLE `reply` ADD CONSTRAINT `reply_message_id_foreign` FOREIGN KEY(`message_id`) REFERENCES `message`(`id`);
 ALTER TABLE `member_notification` ADD CONSTRAINT `member_notification_notification_id_foreign` FOREIGN KEY(`notification_id`) REFERENCES `notification`(`id`);
+ALTER TABLE `message` ADD CONSTRAINT `message_refer_reply_id_foreign` FOREIGN KEY(`refer_reply_id`) REFERENCES `reply`(`id`);
--- scripts/README.md
+++ scripts/README.md
@@ -5,6 +5,3 @@
 
 ## ChangeLog
 - 21.12.19 SQL 스크립트 추가
-- 22.01.03 SQL 스크립트 수정
-  - `file` 테이블 추가 및 `message`, `reply` 테이블에 관련 필드 추가
-  - Join table들의 이름을 member가 앞으로 오도록 고치고 PK 변경

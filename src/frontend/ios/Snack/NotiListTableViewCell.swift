4ee7d5fc1a85ac1ca525acc2a179d2ad2ab12bf2
Geon-Hyeong KIM <show11837@gmail.com>
2022-01-03 11:48:31 +0900

    feat : noti list 화면 스케치 시작

---
+++ src/frontend/ios/Snack/NotiListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotiListTableViewCell.swift
+//  Snack
+//
+//  Created by ghyeongkim-MN on 2022/01/03.
+//
+
+import UIKit
+
+class NotiListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

a51c3442108651fe83c1fac1d9839d7aab2814f6
Jisoo Kim <47516074+SooKim1110@users.noreply.github.com>
2022-01-10 17:43:00 +0900

    Feat/auth/signup (#53)
    * feat: gateway, authServer 프로젝트 초기화
    
    * feat: signup api 추가
    
    * feat: 중복 이메일 검증 로직 추가
    
    - Exception 처리

---
+++ src/backend/authServer/src/main/java/lastpunch/authserver/controller/SignupController.java
@@ -0,0 +1,24 @@
+package lastpunch.authserver.controller;
+
+import lastpunch.authserver.dto.SignupRequest;
+import lastpunch.authserver.service.SignupService;
+import lastpunch.authserver.common.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/signup")
+@RestController
+public class SignupController {
+    private final SignupService signupService;
+    @PostMapping
+    public ResponseEntity<Object> postSignup(@RequestBody SignupRequest signupRequest) {
+        signupService.signup(signupRequest);
+        return Response.toResponseEntity("10000", HttpStatus.OK);
+    }
+}

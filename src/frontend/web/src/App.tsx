5fa823a209d0848d95b401ae268e701bcc22cbed
chahtk <chahtk@gmail.com>
2022-01-10 16:57:19 +0900

    reafctor: route 모듈화
    - public: 로그인 안 된 상태
    - private: 로그인 상태

--- src/frontend/web/src/App.tsx
+++ src/frontend/web/src/App.tsx
@@ -2,34 +2,23 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from './modules';
 import { Route, Routes, useNavigate } from 'react-router-dom';
-import Chat from './components/Chat';
-import MainHeader from './components/MianHeader';
-import LoginPage from './pages/Login';
-import Signup from './pages/Signup';
-import Welcome from './pages/Welcome';
+import PrivateRoute from './routes/Private';
+import PublicRoute from './routes/Public';
 
 function App() {
   const modalActive = useSelector((state: RootState) => state.modal.active);
+  const jwt = sessionStorage.getItem('jwt');
   const navigate = useNavigate();
 
   // jwt가 없으면 login page로 redirect
   useEffect(() => {
-    const jwt = sessionStorage.getItem('jwt');
-    console.log(jwt);
     if (!jwt) navigate('/login');
     else navigate('/', { state: { jwt } });
   }, []);
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Welcome></Welcome>}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/header" element={<MainHeader />}></Route>
-        <Route path="/chat" element={<Chat />}></Route>
-        <Route path="/*" element={<div>not found</div>}></Route>
-      </Routes>
+      {!jwt ? <PublicRoute /> : <PrivateRoute />}
       {modalActive && <div>modal open</div>}
     </>
   );

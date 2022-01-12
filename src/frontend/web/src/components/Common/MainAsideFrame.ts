41b5b4cd03e4b86fa7e83828d7b4d817c05a02aa
chahtk <chahtk@gmail.com>
2022-01-12 18:43:50 +0900

    feat: Aside styled-component

---
+++ src/frontend/web/src/components/Common/MainAsideFrame.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const MainAsideFrame = styled.header`
+  min-width: 260px;
+  background-color: ${(props) => props.theme.color.heavySlack};
+`;
+
+export default MainAsideFrame;

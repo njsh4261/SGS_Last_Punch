003f4fe238edc0e5590595a87a5bda89b9b7e85b
chahtk <chahtk@gmail.com>
2022-01-13 03:17:52 +0900

    feat: ws 생성 UI

--- src/frontend/web/src/pages/CreateWs.tsx
+++ src/frontend/web/src/pages/CreateWs.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import MainHeaderFrame from '../components/Common/MainHeaderFrame';
 import MainAsideFrame from '../components/Common/MainAsideFrame';
-import Chat from '../components/Main/Chat';
+import CreateWsContainer from '../components/CreateWs';
+import { useNavigate } from 'react-router-dom';
 
 const PageLayout = styled.div`
   display: flex;
@@ -32,14 +33,45 @@ const MainAsideWsName = styled.div`
 
 export default function WsCreator() {
   const [wsName, setWsName] = useState('새 워크스페이스');
+  const [channelName, setChannelName] = useState('');
+  const [step, setStep] = useState(1);
+
+  const navigate = useNavigate();
+
+  const nextStepHandler = () => {
+    if (step + 1 === 3) {
+      if (channelName === '') {
+        alert('팀이 사용할 채널 명을 입력하세요');
+        return;
+      }
+      // alert('create workspace!');
+      // call api (create workspace)
+      // redirect this workspace
+      navigate('/welcome'); // 임시
+      return;
+    }
+    setStep((state) => state + 1);
+  };
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === 'ws') setWsName(e.target.value);
+    if (e.target.name === 'channel') setChannelName(e.target.value);
+  };
+
   return (
     <PageLayout>
       <MainHeaderFrame></MainHeaderFrame>
       <Body>
         <MainAsideFrame>
-          <MainAsideWsName>{wsName}ascalksncjacnajkcacssasdasa</MainAsideWsName>
+          <MainAsideWsName>{wsName}</MainAsideWsName>
         </MainAsideFrame>
-        <Chat></Chat>
+        <CreateWsContainer
+          step={step}
+          wsName={wsName}
+          channelName={channelName}
+          inputHandler={inputHandler}
+          nextStepHandler={nextStepHandler}
+        ></CreateWsContainer>
       </Body>
     </PageLayout>
   );

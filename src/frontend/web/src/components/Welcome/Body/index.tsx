ec4b9802f023f264e734bd3830d15900b0d00933
chahtk <chahtk@gmail.com>
2022-01-12 16:51:43 +0900

    feat: workspace list UI

--- src/frontend/web/src/components/Welcome/Body/index.tsx
+++ src/frontend/web/src/components/Welcome/Body/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import WsList from './WsList';
 import CreateWs from './CreateWs';
 
 const SwelcomeBody = styled.main`
   margin: 0 64px 0 64px;
   flex: 1;
+
+  & > * + * {
+    margin-top: 48px;
+  }
 `;
 
 export default function WelcomeBody() {
   return (
     <SwelcomeBody>
+      <WsList></WsList>
       <CreateWs></CreateWs>
     </SwelcomeBody>
   );

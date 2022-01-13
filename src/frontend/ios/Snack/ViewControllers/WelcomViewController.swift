50a324297ab956d6e42e43dfacb04a30266af094
Geon-Hyeong KIM <show11837@gmail.com>
2022-01-14 02:04:21 +0900

    feat: welcome UI 완성

--- src/frontend/ios/Snack/ViewControllers/WelcomViewController.swift
+++ src/frontend/ios/Snack/ViewControllers/WelcomViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import SnapKit
 import Then
 
 class WelcomViewController: UIViewController {
     let disposeBase = DisposeBag()
     
-    let ivLogo = UIImageView()
+    var ivLogo = UIImageView()
     var btnSignIn = UIButton()
     var btnSignUp = UIButton()
     
@@ -34,9 +35,49 @@ class WelcomViewController: UIViewController {
     
     private func attribute() {
         title = "Welcome"
+        
+        ivLogo.image = UIImage(named: "snack")
+        
+        [btnSignIn, btnSignUp].forEach {
+            $0.titleLabel?.font = UIFont(name: "NotoSansKR-Bold", size: 16)
+            $0.setTitleColor(.white, for: .normal)
+            $0.layer.cornerRadius = 6
+        }
+        
+        btnSignIn = btnSignIn.then {
+            $0.setTitle("로그인", for: .normal)
+            $0.backgroundColor = UIColor(named: "snackColor")
+        }
+        btnSignUp = btnSignUp.then {
+            $0.setTitle("회원가입", for: .normal)
+            $0.backgroundColor = .lightGray
+        }
     }
     
     private func layout() {
+        [ivLogo, btnSignIn, btnSignUp].forEach {
+            view.addSubview($0)
+        }
         
+        ivLogo.snp.makeConstraints {
+            $0.width.height.equalTo(130)
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(90)
+        }
+        
+        [btnSignIn, btnSignUp].forEach {
+            $0.snp.makeConstraints {
+                $0.left.right.equalTo(view.safeAreaLayoutGuide).inset(20)
+                $0.height.equalTo(50)
+            }
+        }
+        
+        btnSignIn.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(78)
+        }
+        
+        btnSignUp.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(18)
+        }
     }
 }

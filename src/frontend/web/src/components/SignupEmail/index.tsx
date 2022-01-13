f347f52565c1c79f08395017e3f2180c8e8f3a88
chahtk <chahtk@google.com>
2022-01-13 15:13:45 +0900

    fix: alert 위치 변경 (api -> container)

--- src/frontend/web/src/components/SignupEmail/index.tsx
+++ src/frontend/web/src/components/SignupEmail/index.tsx
@@ -46,8 +46,13 @@ export default function SignupEmailContainer() {
       return alert('비밀번호가 서로 다릅니다');
     }
     const success = await signupAPI(email, pass);
-    navigate('/login'); // 임시
-    // if (success) navigate('/login');
+
+    if (success) navigate('/login');
+    else alert('회원가입 실패');
+
+    // 임시코드 -> 나중에 삭제!!
+    navigate('/login');
+    // 나중에 반드시 삭제!!!
   };
 
   return (

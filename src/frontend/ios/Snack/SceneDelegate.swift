8ce99686cb805cc2248b489589ee76347fe51387
Geon-Hyeong KIM <show11837@gmail.com>
2022-01-14 01:21:04 +0900

    style: 불필요한 부분 코드 및 파일 제거

--- src/frontend/ios/Snack/SceneDelegate.swift
+++ src/frontend/ios/Snack/SceneDelegate.swift
@@ -10,13 +10,13 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    let rootViewModel = LoginViewModel()
+    let rootViewModel = WelcomeViewModel()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         self.window = UIWindow(windowScene: windowScene)
         
-        let rootViewController = LoginViewController()
+        let rootViewController = WelcomViewController()
 //        rootViewController.bind(rootViewModel)
         
         let rootNavigationController = UINavigationController(rootViewController: rootViewController)

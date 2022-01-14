09838107b5201e71013015bb58793bc47a7671ee
chahtk <chahtk@gmail.com>
2022-01-14 14:41:14 +0900

    feat: headerUI of side

--- src/frontend/web/src/components/Main/Aside/index.tsx
+++ src/frontend/web/src/components/Main/Aside/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import MainAsideFrame from '../../Common/MainAsideFrame';
+import AsideHeader from '../../Common/MainAsideHeader';
 
 export default function Aside() {
-  return <MainAsideFrame></MainAsideFrame>;
+  const wsName = 'default'; // todo: redux store
+  return (
+    <MainAsideFrame>
+      <AsideHeader>{wsName} tttttttttttttttt tttttttttttttttt</AsideHeader>
+    </MainAsideFrame>
+  );
 }

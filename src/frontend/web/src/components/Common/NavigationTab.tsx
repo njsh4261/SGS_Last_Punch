ff6f9ca614d518fc47b604459b319af313708c9f
chahtk <chahtk@gmail.com>
2022-01-18 11:47:10 +0900

    feat: navtab proto

--- src/frontend/web/src/components/Common/NavigationTab.tsx
+++ src/frontend/web/src/components/Common/NavigationTab.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { selectChannel } from '../../modules/channel';
+import { useNavigate, useLocation, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 const NavContainer = styled.nav`
@@ -26,33 +28,59 @@ const NavItem = styled.section<{ selected: boolean }>`
   `}
 `;
 
-export default function NavigationTab() {
-  const navigate = useNavigate();
+type NavType = 'CHAT' | 'NOTE' | 'TASK' | 'MEMB';
 
-  // dummy
-  const navItems = [
-    { id: '1', name: 'chat' },
-    { id: '2', name: 'note' },
-    { id: '3', name: 'task' },
-    { id: '4', name: 'memb' },
-  ];
-  const [selectedId, selectId] = useState('1');
+export default function NavigationTab({ mode }: { mode: NavType }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const params = useParams();
+
+  const navItems = ['chat', 'note', 'task', 'memb'];
+
+  let initialState;
+  if (mode === 'CHAT') initialState = 'chat';
+  if (mode === 'NOTE') initialState = 'note';
+  if (mode === 'TASK') initialState = 'task';
+  // member는 페이지가 아니고 모달이므로 초기값이 필요 없음.
+
+  interface StateType {
+    channelName: string;
+  }
+
+  const [selectedItem] = useState(initialState);
   const selectNavHandler = (e: React.MouseEvent<HTMLDivElement>) => {
-    const { id } = e.target as Element;
-    selectId(id);
-    navigate(id);
+    const item = (e.target as Element).id;
+
+    if (item !== selectedItem) {
+      const wsId = params.wsId as string;
+      const channelName = (location.state as StateType).channelName;
+      const channelId = params.channelId as string;
+
+      if (item === 'chat') {
+        dispatch(selectChannel(channelId, channelName, navigate));
+      }
+      // item === 2, go note page
+      if (item === 'note') {
+        navigate('note', { state: { channelName, wsId, mode: 'NOTE' } });
+      }
+      // item === 3, go task page
+      if (item === 'task') {
+        navigate('task', { state: { channelName, wsId, mode: 'TASK' } });
+      }
+    }
   };
 
   return (
     <NavContainer>
       {navItems.map((navItem) => (
         <NavItem
-          id={navItem.id}
-          key={navItem.id}
-          selected={selectedId === navItem.id}
+          id={navItem}
+          key={navItem}
+          selected={selectedItem === navItem}
           onClick={selectNavHandler}
         >
-          {navItem.name}
+          {navItem}
         </NavItem>
       ))}
     </NavContainer>

f1c756b37714dc602c755ba880208f7045552fe9
Geon-Hyeong KIM <show11837@gmail.com>
2022-01-23 12:24:06 +0900

    feat: bind에 필요한 input/output

--- src/frontend/ios/Snack/ViewControllers/WelcomeView/RegisterViewModel.swift
+++ src/frontend/ios/Snack/ViewControllers/WelcomeView/RegisterViewModel.swift
@@ -6,21 +6,44 @@
 //
 
 import RxSwift
+import RxCocoa
+import ProgressHUD
 
 class RegisterViewModel: ViewModelProtocol {
     struct Input {
+        let email = PublishSubject<String>()
+        let code = PublishSubject<String>()
+        let password = PublishSubject<String>()
+        let checkPassword = PublishSubject<String>()
+        let changedEmail = PublishSubject<Void>()
+        let btnSendEmailTapped = PublishSubject<Void>()
+        let btnVerificationTapped = PublishSubject<Void>()
+        let btnSignUpTapped = PublishSubject<Void>()
     }
     
     struct Output {
+        let enableBtnSignUp = PublishRelay<Bool>()
+        let enableBtnSendEmail = PublishRelay<Bool>()
+        let enableBtnVerification = PublishRelay<Bool>()
+        let visibilityCode = PublishRelay<Bool>()
+        let visibilityPassword = PublishRelay<Bool>()
+        let successMessage = PublishRelay<String>()
+        let errorMessage = PublishRelay<String>()
+        let goToLogin = PublishRelay<Void>()
     }
     // MARK: - Public properties
-    let input: Input
-    let output: Output
+    var input = Input()
+    var output = Output()
     
     // MARK: - Private properties
     private let disposeBag = DisposeBag()
     
     // MARK: - Init
+    init() {
+//        Observable.asDriver(input.email)
+//            .
+//
+//            .disposed(by: disposeBag)
         
         
         

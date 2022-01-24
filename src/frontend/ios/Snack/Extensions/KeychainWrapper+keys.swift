8005f0ccb7ba80b0b264653ae5c3146af869fe0e
Geon-Hyeong KIM <show11837@gmail.com>
2022-01-25 01:01:18 +0900

    feat: KeychainWrapper+keys 확장

---
+++ src/frontend/ios/Snack/Extensions/KeychainWrapper+keys.swift
@@ -0,0 +1,14 @@
+//
+//  KeychainWrapper+keys.swift
+//  Snack
+//
+//  Created by ghyeongkim-MN on 2022/01/25.
+//
+
+import SwiftKeychainWrapper
+
+extension KeychainWrapper.Key {
+    static let accessToken: KeychainWrapper.Key = "accessToken"
+    static let refreshToken: KeychainWrapper.Key = "refreshToken"
+    static let id: KeychainWrapper.Key = "id"
+}

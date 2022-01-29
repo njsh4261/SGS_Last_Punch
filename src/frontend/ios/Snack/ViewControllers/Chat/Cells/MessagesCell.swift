f3e143719b73471e6c959717e9182f392bf853c9
Geon-Hyeong KIM <show11837@gmail.com>
2022-01-29 16:54:20 +0900

    style: 파일 이동 (cell 관리)

---
+++ src/frontend/ios/Snack/ViewControllers/Chat/Cells/MessagesCell.swift
@@ -0,0 +1,15 @@
+//
+//  MessagesCell.swift
+//  Snack
+//
+//  Created by ghyeongkim-MN on 2022/01/27.
+//
+
+import UIKit
+
+class MessagesCell: UITableViewCell {
+
+    func bindData(_ messagesView: MessageView, at indexPath: IndexPath) {
+        
+    }
+}

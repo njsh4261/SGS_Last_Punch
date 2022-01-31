222a86971fb9556b8f7989110ff1d31059c5291f
SooKim1110 <ohjskim@naver.com>
2022-01-31 17:46:19 +0900

    fix: cors 메소드, 헤더 허용 설정 추가

--- src/backend/note-server/http-server/src/main/java/lastpunch/notehttpserver/config/WebConfig.java
+++ src/backend/note-server/http-server/src/main/java/lastpunch/notehttpserver/config/WebConfig.java
@@ -1,5 +1,6 @@
 package lastpunch.notehttpserver.config;
 
+import javax.ws.rs.HttpMethod;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000");
+            .allowedOrigins("http://localhost:3000")
+            .allowedHeaders("*")
+            .allowedMethods("*");
     }
 }

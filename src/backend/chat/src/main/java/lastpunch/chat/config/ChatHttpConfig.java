ff63790689b8fce415aa24a37e107a799fa1afcd
Jihyo Kim <njsh4261@gmail.com>
2022-02-03 14:14:57 +0900

    fix: HTTP method를 위한 CORS 설정 추가 (#158)

---
+++ src/backend/chat/src/main/java/lastpunch/chat/config/ChatHttpConfig.java
@@ -0,0 +1,14 @@
+package lastpunch.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ChatHttpConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*");
+    }
+}

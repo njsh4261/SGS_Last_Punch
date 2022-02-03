df3bebc0b7a0571c94f406b75d30346dad194568
njsh4261 <njsh4261@snu.ac.kr>
2022-02-03 14:14:31 +0900

    fix: HTTP method를 위한 CORS 설정 추가

--- src/backend/chat/src/main/java/lastpunch/chat/config/ChatSocketConfig.java
+++ src/backend/chat/src/main/java/lastpunch/chat/config/ChatSocketConfig.java
@@ -3,16 +3,13 @@ package lastpunch.chat.config;
 import lastpunch.chat.common.ChatConstant;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class ChatConfig implements WebSocketMessageBrokerConfigurer{
-    
-    
+public class ChatSocketConfig implements WebSocketMessageBrokerConfigurer{
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint(ChatConstant.ENDPOINT)

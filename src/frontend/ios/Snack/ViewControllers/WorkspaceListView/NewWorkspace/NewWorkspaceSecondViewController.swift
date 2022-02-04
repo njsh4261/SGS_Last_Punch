73e680af259f4934cece73c2cd1841e31634a571
Geon-Hyeong KIM <show11837@gmail.com>
2022-02-05 01:34:42 +0900

    refactor: tabBarContoller 생성 class

--- src/frontend/ios/Snack/ViewControllers/WorkspaceListView/NewWorkspace/NewWorkspaceSecondViewController.swift
+++ src/frontend/ios/Snack/ViewControllers/WorkspaceListView/NewWorkspace/NewWorkspaceSecondViewController.swift
@@ -91,29 +91,7 @@ class NewWorkspaceSecondViewController: UIViewController {
         pvc.dismiss(animated: true) {
             if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate {
                 guard let workspaceId: String = KeychainWrapper.standard[.workspaceId] else { return }
-
-                let homeView = HomeViewController()
-                let DMView = DirectMessageListViewController(workspaceId: workspaceId.description)
-                DMView.bind(with: DirectMessageListViewModel())
-                let profileView = SettingsViewController(nibName: "SettingsView", bundle: nil)
-                
-                let navController0 = NavigationController(rootViewController: homeView)
-                let navController1 = NavigationController(rootViewController: DMView)
-                let navController4 = NavigationController(rootViewController: profileView)
-                
-                let tabBarController = UITabBarController()
-                tabBarController.viewControllers = [navController0, navController1, navController4]
-                tabBarController.tabBar.isTranslucent = false
-                tabBarController.tabBar.tintColor = UIColor(named: "snackColor")
-                tabBarController.modalPresentationStyle = .fullScreen
-                tabBarController.selectedIndex = App.DefaultTab
-                
-                if #available(iOS 15.0, *) {
-                    let appearance = UITabBarAppearance()
-                    appearance.configureWithOpaqueBackground()
-                    tabBarController.tabBar.standardAppearance = appearance
-                    tabBarController.tabBar.scrollEdgeAppearance = appearance
-                }
+                let tabBarController = CreateHomeTabController().getTabController(workspaceId.description)
 
                 sceneDelegate.welcomeViewController.present(tabBarController, animated: true, completion: nil)
             }

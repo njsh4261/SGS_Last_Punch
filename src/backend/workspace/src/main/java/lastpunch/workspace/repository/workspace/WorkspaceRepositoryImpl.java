e8600d01800f8ee669b4c3e742c7c22b50c0b094
jihyokim <jihyokim@smilegate.com>
2022-02-04 18:34:55 +0900

    fix: Account 테이블의 displayname 삭제 사항을 워크스페이스 API에 반영

--- src/backend/workspace/src/main/java/lastpunch/workspace/repository/workspace/WorkspaceRepositoryImpl.java
+++ src/backend/workspace/src/main/java/lastpunch/workspace/repository/workspace/WorkspaceRepositoryImpl.java
@@ -62,7 +62,7 @@ public class WorkspaceRepositoryImpl implements WorkspaceRepositoryCustom{
     public Page<Account.ExportDto> getMembers(Long id, Pageable pageable){
         List<Account.ExportDto> results = jpaQueryFactory
                 .select(new QAccount_ExportDto(
-                        account.id, account.email, account.name, account.displayname,
+                        account.id, account.email, account.name,
                         account.description, account.phone, account.country, account.language,
                         account.settings, account.status, account.createdt, account.modifydt
                 ))

84fbc3342bb61319974183f2d4ef9830afe98054
chahtk <chahtk@gmail.com>
2022-02-05 05:54:59 +0900

    fix: sessionStorage -> local

--- src/frontend/web/src/Api/reissue.ts
+++ src/frontend/web/src/Api/reissue.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { URL, TOKEN, ENDPOINT, RESPONSE } from '../constant';
 
 export default async function reissueAPI() {
-  const refreshToken = sessionStorage.getItem(TOKEN.REFRESH);
+  const refreshToken = localStorage.getItem(TOKEN.REFRESH);
   if (!refreshToken) {
     console.error('no refresh token');
     return;

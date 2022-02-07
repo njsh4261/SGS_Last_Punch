ba7f052a3d360331d4a4c673d35a3a09cf9cfa6a
chahtk <chahtk@gmail.com>
2022-02-07 13:12:54 +0900

    feat: dropdown item

--- src/frontend/web/src/components/Main/Chat/Dropdown.tsx
+++ src/frontend/web/src/components/Main/Chat/Dropdown.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
   position: absolute;
@@ -8,10 +9,28 @@ const Container = styled.div`
   right: 0;
   transform: translateY(50%);
   width: 100px;
-  height: 100px;
   border: 1px solid darkgray;
   border-radius: 8px;
   box-shadow: 0 8px 14px rgba(0, 0, 0, 0.35);
+  overflow: hidden;
 `;
 
-export default Container;
+const Layer = styled.article`
+  border-bottom: 1px solid lightgray;
+  text-align: center;
+  padding: 4px 0;
+  :hover {
+    cursor: pointer;
+    background-color: lightgray;
+  }
+`;
+
+export default function Dropdown({ id }: { id: string }) {
+  const navigate = useNavigate();
+  return (
+    <Container id={id}>
+      <Layer onClick={() => navigate('/')}>Home</Layer>
+      <Layer onClick={() => alert('todo: profile')}>Profile</Layer>
+    </Container>
+  );
+}

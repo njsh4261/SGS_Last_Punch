26116da626f6316ba7b13551ce41557ec04af4fa
chahtk <chahtk@gmail.com>
2022-02-08 09:19:53 +0900

    fix: ws dropdown position

--- src/frontend/web/src/components/Main/Aside/DropdownWorkspace.tsx
+++ src/frontend/web/src/components/Main/Aside/DropdownWorkspace.tsx
@@ -6,7 +6,7 @@ import { Container, Layer } from '../../Common/DropdownComponent';
 export default function DropdownWorkspace({ id }: { id?: string }) {
   const navigate = useNavigate();
   return (
-    <Container id={id} moveX={true}>
+    <Container id={id} left={true}>
       <Layer onClick={() => alert('todo: invite')}>invite</Layer>
       <Layer onClick={() => alert('todo: exit workspace')} color="red">
         Exit

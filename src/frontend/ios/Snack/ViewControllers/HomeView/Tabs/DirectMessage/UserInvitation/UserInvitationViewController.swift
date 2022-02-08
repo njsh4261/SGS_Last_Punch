d97a45002df2ee82f59b3274e13fb60264df0ada
Geon-Hyeong KIM <show11837@gmail.com>
2022-02-09 04:21:42 +0900

    style: footer text 색상

--- src/frontend/ios/Snack/ViewControllers/HomeView/Tabs/DirectMessage/UserInvitation/UserInvitationViewController.swift
+++ src/frontend/ios/Snack/ViewControllers/HomeView/Tabs/DirectMessage/UserInvitation/UserInvitationViewController.swift
@@ -18,7 +18,7 @@ class UserInvitationViewController: UIViewController {
     // MARK: - Properties
     private let disposeBag = DisposeBag()
     private var dataSource: RxTableViewSectionedReloadDataSource<UserInvitationSection.Model>!
-    private let FOOTER_HEIGHT = 80
+    private let FOOTER_HEIGHT = 100
     
     // MARK: - UI
     private var tableView = UITableView()
@@ -26,6 +26,7 @@ class UserInvitationViewController: UIViewController {
     private var lblLinkDesciption = UILabel()
     private var lblTip = UILabel()
     private var btnSend = UIBarButtonItem()
+    private var ivSnack = UIImageView()
         
     override init(nibName nibNameOrNil: String? = nil, bundle nibBundleOrNil: Bundle? = nil) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -109,17 +110,22 @@ class UserInvitationViewController: UIViewController {
         lblLinkDesciption = lblLinkDesciption.then {
             $0.text = "공유할 링크를 생성합니다. 누구나 이 링크를 사용해\n워크스페이스에 참여할 수 있습니다."
             $0.lineBreakMode = .byWordWrapping
-            $0.textColor = .darkGray
+            $0.textColor = .secondaryLabel
             $0.font = UIFont(name: "NotoSansKR-Bold", size: 13)
             $0.numberOfLines = 2
             $0.textAlignment = .left
         }
         
         lblTip = lblTip.then {
-            $0.text = "Snack은 다른 사용자들과 함께 할 때 더 유용합니다."
-            $0.textColor = .darkGray
+            $0.text = "Snack은 다른 사용자들과 함께 할 때 더 유용합니다 :D"
+            $0.textColor = .tertiaryLabel
             $0.font = UIFont(name: "NotoSansKR-Bold", size: 13)
             $0.textAlignment = .left
+        }
+        
+        ivSnack = ivSnack.then {
+            $0.frame = CGRect(x: 0, y: 0, width: 2, height: 2)
+            $0.image = UIImage(named: "snack_darkGray")
         }
     }
     
@@ -135,16 +141,23 @@ class UserInvitationViewController: UIViewController {
             $0.height.equalTo(FOOTER_HEIGHT)
         }
         
-        [lblLinkDesciption, lblTip].forEach {
+        [lblLinkDesciption, lblTip, ivSnack].forEach {
             footerView.addSubview($0)
-            $0.snp.makeConstraints {
-                $0.left.right.equalToSuperview().inset(15)
-            }
         }
         
         lblLinkDesciption.snp.makeConstraints {
             $0.top.equalToSuperview()
+            $0.left.right.equalToSuperview().inset(15)
             $0.bottom.equalTo(lblTip.snp.top).offset(-20)
+        }
+        
+        lblTip.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(15)
+            $0.right.equalTo(ivSnack.snp.left)
+        }
+
+        ivSnack.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
         }
     }
 }

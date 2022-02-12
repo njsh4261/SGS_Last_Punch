11911889ec43bd8f692f9c739f0a8633fe274d44
chahtk <chahtk@gmail.com>
2022-02-12 22:24:54 +0900

    style: file name

--- src/frontend/web/src/pages/Signup.tsx
+++ src/frontend/web/src/pages/Signup.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import SignupEmailContainer from '../components/SignupEmail';
+import SignupEmailContainer from '../components/Signup';
 
 const LoginLayout = styled.main`
   position: absolute;

1d63f87f3a968b8784be331c456f767981af8241
chahtk <chahtk@gmail.com>
2022-02-14 17:10:46 +0900

    feat: display channel data

--- src/frontend/web/src/components/Common/ModalMain/ModalInfo.tsx
+++ src/frontend/web/src/components/Common/ModalMain/ModalInfo.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+
+import { RootState } from '../../../modules';
+import { exitChannelAPI, getChannelInfoAPI } from '../../../Api/channel';
+import { exitWsAPI } from '../../../Api/workspace';
+import { IChannel } from '../../../../types/channel.type';
+import { useParams } from 'react-router-dom';
+import Loading from '../Loading';
 
 const Container = styled.article`
   margin: 18px 28px;
@@ -43,33 +51,71 @@ interface Props {
 }
 
 export default function ModalInfo({ type }: Props) {
+  const params = useParams();
+  const [channel, setChannel] = useState<IChannel | undefined>();
+  const user = useSelector((state: RootState) => state.user);
+
+  const exitHandler = async () => {
+    let response;
+    if (type === 'channel') {
+      response = await exitChannelAPI(user.id, +channel!.id);
+    }
+    // else {
+    //   response = await exitWsAPI(wsId, user.id);
+    // }
+    console.log({ response });
+  };
+
+  const getInfoHandler = async () => {
+    if (params.channelId) {
+      const response = await getChannelInfoAPI(params.channelId);
+      setChannel(response.channel);
+    }
+  };
+
+  useEffect(() => {
+    getInfoHandler();
+  }, []);
+
   return (
-    <Container>
-      <ItemBox>
-        <ItemL1>
-          <div>주제</div>
-          <ItemL1Edit>편집</ItemL1Edit>
-        </ItemL1>
-        <ItemL2>주제 추가</ItemL2>
-      </ItemBox>
-      <ItemBox>
-        <ItemL1>
-          <div>설명</div>
-          <ItemL1Edit>편집</ItemL1Edit>
-        </ItemL1>
-        <ItemL2>설명 추가</ItemL2>
-      </ItemBox>
-      <ItemBox>
-        <ItemL1>
-          <div>소유자</div>
-        </ItemL1>
-        <ItemL2>소유자: [소유자이름], 생성 날짜: [createDt]</ItemL2>
-      </ItemBox>
-      <ItemBox>
-        <ItemL1>
-          {type === 'channel' ? '채널' : '워크스페이스'}에서 나가기
-        </ItemL1>
-      </ItemBox>
-    </Container>
+    <>
+      {!channel ? (
+        <Loading></Loading>
+      ) : (
+        <Container>
+          <ItemBox>
+            <ItemL1>
+              <div>주제</div>
+              {/* <ItemL1Edit>편집</ItemL1Edit> */}
+            </ItemL1>
+            <ItemL2>
+              {channel.topic !== '' ? channel.topic : '주제 추가'}
+            </ItemL2>
+          </ItemBox>
+          <ItemBox>
+            <ItemL1>
+              <div>설명</div>
+              {/* <ItemL1Edit>편집</ItemL1Edit> */}
+            </ItemL1>
+            <ItemL2>
+              {channel.description !== '' ? channel.description : '설명 추가'}
+            </ItemL2>
+          </ItemBox>
+          <ItemBox>
+            <ItemL1>
+              <div>소유자</div>
+            </ItemL1>
+            <ItemL2>
+              소유자: [소유자이름], 생성 날짜: {channel.createDt.split(' ')[0]}
+            </ItemL2>
+          </ItemBox>
+          <ItemBox onClick={exitHandler}>
+            <ItemL1>
+              {type === 'channel' ? '채널' : '워크스페이스'}에서 나가기
+            </ItemL1>
+          </ItemBox>
+        </Container>
+      )}
+    </>
   );
 }

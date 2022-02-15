86e29c6d5ea51f7c1458176751b421d8853795e6
NS-GLT <njsh4261@gmail.com>
2022-02-15 11:01:05 +0900

    fix: accountWorkspace & accountChannel 관련 오타 수정 및 주석 추가

--- src/backend/workspace/src/main/java/lastpunch/workspace/controller/AccountChannelController.java
+++ src/backend/workspace/src/main/java/lastpunch/workspace/controller/AccountChannelController.java
@@ -17,21 +17,26 @@ public class AccountChannelController{
     public AccountChannelController(AccountChannelService accountChannelService){
         this.accountChannelService = accountChannelService;
     }
-    
+
+    // 채널에 새로운 멤버를 추가
     @PostMapping
     public ResponseEntity<Object> add(@RequestBody AccountChannel.Dto accountChannelDto){
         return Response.ok(ServerCode.WORKSPACE, accountChannelService.add(accountChannelDto));
     }
 
+    // 채널 내 멤버의 권한을 변경
+    // 권한을 가진 유저가 다른 멤버의 권한을 바꾸거나 채널 소유자가 다른 멤버에게 소유권을 양도할 때 사용
     @PutMapping
     public ResponseEntity<Object> edit(@RequestBody AccountChannel.Dto accountChannelDto){
         return Response.ok(ServerCode.WORKSPACE, accountChannelService.edit(accountChannelDto));
     }
 
+    // 채널에서 멤버를 삭제
+    // 권한을 가진 멤버가 다른 멤버를 강퇴하거나 채널에서 자진 탈퇴할 때 사용
     @DeleteMapping
     public ResponseEntity<Object> delete(
             @RequestParam(value = "accountId") Long accountId,
-            @RequestParam(value = "workspaceId") Long workspaceId){
-        return Response.ok(ServerCode.WORKSPACE, accountChannelService.delete(accountId, workspaceId));
+            @RequestParam(value = "channelId") Long channelId){
+        return Response.ok(ServerCode.WORKSPACE, accountChannelService.delete(accountId, channelId));
     }
 }
--- src/backend/workspace/src/main/java/lastpunch/workspace/controller/AccountWorkspaceController.java
+++ src/backend/workspace/src/main/java/lastpunch/workspace/controller/AccountWorkspaceController.java
@@ -19,16 +19,21 @@ public class AccountWorkspaceController{
         this.accountWorkspaceService = accountWorkspaceService;
     }
 
+    // 워크스페이스에 새로운 멤버를 추가
     @PostMapping
     public ResponseEntity<Object> add(@RequestBody AccountWorkspace.Dto accountWorkspaceDto){
         return Response.ok(ServerCode.WORKSPACE, accountWorkspaceService.add(accountWorkspaceDto));
     }
-    
+
+    // 워크스페이스 내 멤버의 권한을 변경
+    // 권한을 가진 유저가 다른 멤버의 권한을 바꾸거나 워크스페이스 소유자가 다른 멤버에게 소유권을 양도할 때 사용
     @PutMapping
     public ResponseEntity<Object> edit(@RequestBody AccountWorkspace.Dto accountWorkspaceDto){
         return Response.ok(ServerCode.WORKSPACE, accountWorkspaceService.edit(accountWorkspaceDto));
     }
 
+    // 워크스페이스에서 멤버를 삭제
+    // 권한을 가진 멤버가 다른 멤버를 강퇴하거나 워크스페이스에서 자진 탈퇴할 때 사용
     @DeleteMapping
     public ResponseEntity<Object> delete(
             @RequestParam(value = "accountId") Long accountId,

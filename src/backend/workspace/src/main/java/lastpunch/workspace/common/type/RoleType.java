e6f61d837950a84ecfb652cb843ee257d0743664
njsh4261 <njsh4261@snu.ac.kr>
2022-02-17 01:47:21 +0900

    feat: RoleType mapper 메소드 추가

--- src/backend/workspace/src/main/java/lastpunch/workspace/common/type/RoleType.java
+++ src/backend/workspace/src/main/java/lastpunch/workspace/common/type/RoleType.java
@@ -1,5 +1,7 @@
 package lastpunch.workspace.common.type;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,5 +9,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum RoleType{
     NORMAL_USER(1L), ADMIN(2L), OWNER(3L);
-    private Long id;
+    private final Long id;
+    
+    private static final Map<Long, RoleType> map = new ConcurrentHashMap<>();
+    static {
+        for(RoleType roleType: RoleType.values()){
+            map.put(roleType.getId(), roleType);
+        }
+    }
+    
+    public static RoleType toEnum(Long id){
+        return map.getOrDefault(id, RoleType.NORMAL_USER);
+    }
 }

503950e01e5f813a8d08d06701d3072014aa79cb
chahtk <chahtk@gmail.com>
2022-02-17 21:38:07 +0900

    feat: profile modal

--- src/frontend/web/src/components/Main/Chat/DropdownSetting.tsx
+++ src/frontend/web/src/components/Main/Chat/DropdownSetting.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import logout from '../../../util/logout';
-import { useNavigate } from 'react-router-dom';
 import { Container, Layer } from '../../Common/DropdownComponent';
+import { openModal } from '../../../modules/modal';
 
 export default function Dropdown({ id }: { id: string }) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   return (
     <Container id={id}>
       <Layer onClick={() => navigate('/')}>Home</Layer>
-      <Layer onClick={() => alert('todo: profile')}>Profile</Layer>
+      <Layer onClick={() => dispatch(openModal('profile'))}>Profile</Layer>
       <Layer onClick={() => logout()} color="red">
         Logout
       </Layer>

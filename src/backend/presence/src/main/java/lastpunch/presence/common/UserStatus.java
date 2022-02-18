ae84d2f12c66a7a814def23c39e2f82b3f4f74e1
Jihyo Kim <njsh4261@snu.ac.kr>
2022-02-18 13:58:10 +0900

    fix: 사용자 세션이 다시 연결되는 경우 사용자 상태를 업데이트하지 않음 (#324)

--- src/backend/presence/src/main/java/lastpunch/presence/common/UserStatus.java
+++ src/backend/presence/src/main/java/lastpunch/presence/common/UserStatus.java
@@ -3,7 +3,20 @@ package lastpunch.presence.common;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum UserStatus{
-    ONLINE, ABSENT, BUSY, OFFLINE, DISCONNECT, UNKNOWN;
+    // 세션 연결
+    CONNECT,        // 세션이 연결 및 재연결된 상황을 명시 (ONLINE과 구분)
+    
+    // 세션 연결 중 사용자 상태 변경
+    ONLINE,         // 사용자가 본인의 상태를 '온라인'으로 표시
+    ABSENT,         // 사용자가 본인의 상태를 '부재 중'으로 표시
+    BUSY,           // 사용자가 본인의 상태를 '다른 용무 중'으로 표시
+    OFFLINE,        // 사용자가 본인의 상태를 '오프라인'으로 표시
+    
+    // 세션 종료
+    DISCONNECT,     // 세션이 종료된 상황을 명시 (OFFLINE과 구분)
+    
+    // 비정상 상태
+    UNKNOWN;        // 사용자 상태가 메시지에 정상적으로 명시되지 않은 경우
     
     @JsonCreator
     public static UserStatus toEnum(String s){
